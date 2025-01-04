1238eced33f3193645033c9b03c71d1ee901782f
WITHOUTcorp <withoutcorp@gmail.com>
2025-01-04 16:56:01 +0800

    Create openScript.js

---
+++ openScript.js
@@ -0,0 +1,10 @@
+var startButton = document
+  .querySelector("#openScript")
+  .addEventListener("click", () => {
+    this.openScript();
+  });
+
+// Function that start game
+openScript = () => {
+  console.log("Game is starting");
+};
