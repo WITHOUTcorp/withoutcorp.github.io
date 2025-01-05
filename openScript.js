a46d5cbee7178c802e1e0828b18c4ff5030c3241
WITHOUTcorp <withoutcorp@gmail.com>
2025-01-05 12:03:34 +0800

    Update openScript.js

--- openScript.js
+++ openScript.js
@@ -7,4 +7,15 @@ var startButton = document
 // Function that start game
 openScript = () => {
   console.log("Placeholder");
+  
+  // Let us open our database
+  let db;
+  const DB_VERSION = 21;
+  const DB_STORE_NAME = "FILE_DATA";
+  const tx = db.transaction(DB_STORE_NAME, /* '/saves' */ 'readwrite')
+	const store = tx.objectStore(DB_STORE_NAME /* '/saves' */)
+	const val = store.get("/saves/with.scr").then((result) => {
+    let newStr = Buffer.from(Object.values(result.contents)).toString()
+    /* result */ newStr.contents.split(/\n/).forEach((el) => { let p = document.createElement('p'); let text = document.createTextNode(el); p.appendChild(text); main.appendChild(p)})
+  })
 };
