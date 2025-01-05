94b06c5990e1f8e4a880ce8309e9e5939460324d
WITHOUTcorp <withoutcorp@gmail.com>
2025-01-05 12:05:47 +0800

    Update openScript.js

--- openScript.js
+++ openScript.js
@@ -12,10 +12,10 @@ openScript = () => {
   let db;
   const DB_VERSION = 21;
   const DB_STORE_NAME = "FILE_DATA";
-  const tx = db.transaction(DB_STORE_NAME, /* '/saves' */ 'readwrite')
-	const store = tx.objectStore(DB_STORE_NAME /* '/saves' */)
+  const tx = db.transaction(DB_STORE_NAME, /* '/saves' */ 'readwrite');
+  const store = tx.objectStore(DB_STORE_NAME /* '/saves' */)
 	const val = store.get("/saves/with.scr").then((result) => {
     let newStr = Buffer.from(Object.values(result.contents)).toString()
     /* result */ newStr.contents.split(/\n/).forEach((el) => { let p = document.createElement('p'); let text = document.createTextNode(el); p.appendChild(text); main.appendChild(p)})
-  })
+  });
 };
