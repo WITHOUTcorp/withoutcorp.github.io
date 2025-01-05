9c2fcc58559d6a867f8397ed5bb002501179e375
WITHOUTcorp <withoutcorp@gmail.com>
2025-01-05 10:04:24 +0800

    Update openScript.js

--- openScript.js
+++ openScript.js
@@ -6,9 +6,5 @@ var startButton = document
 
 // Function that start game
 openScript = () => {
-  console.log("Game is starting");
-  let db;
-  
-  // Let us open our database
-  const DBOpenRequest = window.indexedDB.open("/saves/with.scr");
+  console.log("Placeholder");
 };
